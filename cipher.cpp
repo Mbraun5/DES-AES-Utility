9bea4f46cea1e7d289026472946380f0d3eab3ce
Matthew Braun <braun.matthewwork@gmail.com>
2020-04-09 12:27:45 -0700

    added error checking for valid key

--- cipher.cpp
+++ cipher.cpp
@@ -20,35 +20,44 @@ int main(int argc, char** argv)
 	ifstream infile;
 	ofstream ofile;
 
+	/** 
+	 * If argument count != 6, not every parameter was added correctly.
+	 * If argument count is 2 and the 2nd argument is 'help', omit the 'Invalid parameter count' line
+	 */
 	if ((argc == 2 && strcmp(argv[1], "help") == 0 || argc != 6)) {
 		if (!argv[1] || strcmp(argv[1], "help") != 0) { fprintf(stderr, "ERROR! Invalid parameter count!\n"); }
 		print_usage();
 		exit(-1);
 	}
-	else {
-		if (strcmp(argv[1],"DES") == 0) { cipher = new DES(); }
-		else if (strcmp(argv[1], "AES") == 0) { cipher = new AES(); }
-		else {
-			fprintf(stderr, "Invalid <CIPHER NAME>. run ./cipher help for help.\n");
-			exit(-1);
-		}
-	}
 
-	/* Error checks */
-	if(!cipher)
-	{
+	/* Set the cipher using flag from 2nd command line argument */
+	if (strcmp(argv[1],"DES") == 0) { cipher = new DES(); }
+	else if (strcmp(argv[1], "AES") == 0) { cipher = new AES(); }
+	else {		/* Only DES/AES are supported */
+		fprintf(stderr, "Invalid <CIPHER NAME> - must be one of DES/AES. Run ./cipher help for help.\n");
+		exit(-1);
+	}
+	if(!cipher) {  /* Sanity check to make sure cipher was created properly */
 		fprintf(stderr, "ERROR [%s %s %d]: could not allocate memory\n",	
 		__FILE__, __FUNCTION__, __LINE__);
 		exit(-1);
 	}
-	
-	/* Set the encryption key
-	 * A valid key comprises 16 hexidecimal
-	 * characters. Below is one example.
-	 * Your program should take input from
-	 * command line.
+
+	/** 
+	 * Set the encryption/decryption key for the cipher. 
+	 * A valid key comprises 16 hexidecimal characters
 	 */
-	cipher->setKey((unsigned char*)"0123456789abcdef");
+	if (cipher->setKey((unsigned char*)argv[2])) {
+		fprintf(stderr, "Valid key!\n");
+	}
+	else {
+		fprintf(stderr, "Invalid <KEY> - must be a valid 16-length hexidecimal literal. Run ./cipher help for help.\n");
+		exit(-1);
+	}
+	//cipher->setKey((unsigned char*)"0123456789abcdef");
+
+	/* Error checks */
+
 	
 	/* Perform encryption */
 	//string cipherText = cipher->encrypt("hello world");
