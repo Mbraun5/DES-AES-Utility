92d86e8d26ddd4ed2e7d778c9f33c5bd8d549c59
Matthew Braun <braun.matthewwork@gmail.com>
2020-04-10 15:48:33 -0700

    moved python generation script to tests

--- tests/generate_large_file.py
+++ tests/generate_large_file.py
@@ -13,6 +13,10 @@ with open('large.txt', 'w') as f:
 def generate_str():
     return "".join([chr(random.randrange(33,126)) for _ in range(10000)])
 
+with open('large1.txt', 'w') as f:
+    for _ in range(2000):
+        f.write(generate_str())
+
 with open('large2.txt', 'w') as f:
     for _ in range(2000):
         f.write(generate_str())
